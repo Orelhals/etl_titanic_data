ae798adc110adae2f9fba22e7028e9cdd9a83bf1
Arthur P <arthurstt22@gmail.com>
2025-10-31 13:53:27 -0300

    Fix: Enhance main function to display extracted data and processed data with improved output messages

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from extract import extract_data
+from transform import transform_data
 # from transform import clean_data
 # from load import load_data
 
 def main():
     raw = extract_data()
-    print("First 5 records:\n", raw.head())
+    print("Dados extraídos:\n")
+    print(raw.head())
+
+    clean = transform_data(raw)
+    print("Dados em tratamento:\n")
+    print(clean.head())
     
     # clean = clean_data(raw)
     # load_data(clean)
